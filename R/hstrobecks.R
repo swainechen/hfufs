014fabecfafc5b691492071c5484b3a6a87d850c
Swaine Chen <slchen@gis.a-star.edu.sg>
2018-11-20 18:11:27 +0800

    Initial insertion of roxygen2 documentation

--- R/hstrobecks.R
+++ R/hstrobecks.R
@@ -1,8 +1,42 @@
-#
-# Swaine Chen; swainechen@gmail.com
-# https://github.com/swainechen/hfufs
-# MIT License
-#
+#' Calculate Strobeck's S for arbitrary parameters
+#'
+#' Returns Strobeck's S statistic.
+#'
+#' Strobeck's S is a population genetics statistic that is used to detect
+#' population structure. It is related conceptually (and computationally) to
+#' Fu's Fs, which has been argued to be a more appropriate statistic due to
+#' it's higher dynamic range, owing to the use of a logit transformation for
+#' Fu's Fs. In cases where Fu's Fs becomes more negative, Strobeck's S
+#' approaches 1.
+#'
+#' Computationally, Strobeck's S and Fu's Fs can be defined in terms of Stirling
+#' numbers of the first kind. These numbers can grow large very quickly.
+#'
+#' `hstrobecks` calculates Strobeck's S directly using Stirling numbers,
+#' switching to a logarithmic esimator for Stirling numbers (implemented in the
+#' lstirling function) when needed to avoid overflow errors.
+#'
+#' @param n The number of total sequences/individuals
+#' @param k The number of unique alleles
+#' @param theta The average pairwise nucleotide divergence
+#' @return Strobeck's S
+#' @seealso `hfufs` for a Fu's Fs calculator and `lstirling` for the Stirling
+#' number estimator
+#' @export
+#' @references
+#' Fu, Y.X. (1996) New statistical tests of neutrality for DNA samples from
+#' a population. Genetics 143:557-70
+#' Strobeck, C. (1987) Average number of nucleotide differences in a sample
+#' from a single subpopulation: a test for population subdivision. Genetics
+#' 117:149-53
+#'
+#' @examples
+#' n <- 100
+#' k <- 30
+#' theta <- 12.345
+#' hstrobecks(n, k, theta)
+#' # 0.7576966
+#'
 hstrobecks <- function(n, k, theta) {
   # Strobeck's S is prob of k alleles or fewer, Fu's Sp is k alleles or greater
   # if k == 0, then Strobeck's S is 0

78296d8cd4cbbd664e83f5ea0c35ffa58f9d72fe
Swaine Chen <slchen@gis.a-star.edu.sg>
2018-11-20 18:27:25 +0800

    Initial insertion of roxygen2 documentation

--- R/stirmat.R
+++ R/stirmat.R
@@ -1,8 +1,38 @@
-#
-# Swaine Chen; swainechen@gmail.com
-# https://github.com/swainechen/hfufs
-# MIT License
-#
+#' Calculate a matrix of Stirling numbers of the first kind
+#'
+#' Returns a matrix of Stirling numbers of the first kind
+#'
+#' Stirling numbers of the first (and second) kind are useful numbers for
+#' combinatorics. In the context of sequence analysis, Stirling numbers of the
+#' first kind have entered into the calculation of statistics that measure
+#' expected numbers of alleles in a population.
+#'
+#' Stirling numbers are characterized by two parameters, `n` and `m` (or `n`
+#' and `k` in some literature). `stirmat` returns an n by m matrix of all the
+#' respective Stirling numbers using a recursion relationship.
+#'
+#' `stirmat` is a direct port of the code from James Cai's PGEToolbox, which
+#' was originally written for Matlab.
+#'
+#' @param n The first parameter for Stirling numbers
+#' @param m The second parameter for Stirling numbers
+#' @return A matrix with n rows and m columns, the elements of which are
+#' Stirling numbers of the first kind, for all combinations of n and m up to
+#' the supplied n and m
+#' and column, respectively
+#' @seealso `lstirling` for a logarithmic estimator of large Stirling numbers
+#' @export
+#' @references
+#' Cai, J.J. (2008) PGEToolsbox: A Matlab toolbox for population genetics and
+#' evolution. J Hered 99(4):438-40
+#'
+#' @examples
+#' n <- 20
+#' m <- 10
+#' stirling_matrix <- stirmat(n, m)
+#' stirling_matrix[n,m]
+#' # 3.819221e+14
+#'
 stirmat <- function(n, m) {
   # from James Cai PGEToolbox
   if (n <= 0 | m <= 0) {

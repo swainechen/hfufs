7301f18b6494f2b06e6fd8654850287ff4b385b3
Swaine Chen <slchen@gis.a-star.edu.sg>
2020-02-18 16:10:58 +0800

    Cleanup variables, some documentation

--- R/hf.alignment.stats.R
+++ R/hf.alignment.stats.R
@@ -2,29 +2,37 @@
 #'
 #' Takes in a PopGenome GENOME object (from PopGenome::readData) and
 #' calculates basic population genetics statistics. Uses afufs if needed.
+#' Returns a data frame with most of the statistics. The original GENOME
+#' object is modified as normal by the PopGenome functions.
+#'
+#' Runs these functions from PopGenome:
+#'   diversity.stats
+#'   neutrality.stats
 #'
 #' @export
 #'
+#' @examples
+#' \dontrun {
+#' fasta_file <- "name_of_aligned.fasta"
+#' pg.object <- hf.readData(fasta_file)
+#' pg.dataframe <- hf.alignment.stats(pg.object)
+#' }
 #'
-#'
-hf.alignment.stats <- function(x, slide=F, width=1000, step=500) {
+hf.alignment.stats <- function(go, slide=F, window=1000, step=500) {
   # takes in a GENOME object as from PopGenome readData
-  numindividuals <- length(get.individuals(d)[[1]])
-  single_value <- 1/numindividuals
-  if(summary(x)[2]=="GENOME") {
-    d <- x
+  if(summary(go)[2]=="GENOME") {
+    numindividuals <- length(get.individuals(go)[[1]])
+    single_value <- 1/numindividuals
     if (slide) {
-      window_width <- 1000
-      window_step <- 500
-      slide_d <- sliding.window.transform(d, width=window_width, jump=window_step, type=2, whole.data=T)
-      slide_d <- diversity.stats(slide_d, pi=T)
-      slide_d <- neutrality.stats(slide_d, detail=T, do.R2=T)
-      n <- data.frame(get.neutrality(slide_d)[[1]])
-      n$x.start <- as.numeric(sapply(strsplit(slide_d@region.names, split=" - "), "[")[1,])
-      n$x.end <- as.numeric(sapply(strsplit(sub(" :", "", slide_d@region.names), split=" - "), "[")[2,])
+      slide_go <- sliding.window.transform(go, width=window, jump=step, type=2, whole.data=T)
+      slide_go <- diversity.stats(slide_go, pi=T)
+      slide_go <- neutrality.stats(slide_go, detail=T, do.R2=T)
+      n <- data.frame(get.neutrality(slide_go)[[1]])
+      n$x.start <- as.numeric(sapply(strsplit(slide_go@region.names, split=" - "), "[")[1,])
+      n$x.end <- as.numeric(sapply(strsplit(sub(" :", "", slide_go@region.names), split=" - "), "[")[2,])
       n$numindividuals <- numindividuals
-      n$pi <- get.diversity(slide_d)[[1]][,3]
-      h <- slide_d@region.stats@haplotype.counts
+      n$pi <- get.diversity(slide_go)[[1]][,3]
+      h <- slide_go@region.stats@haplotype.counts
       n$n.sequences <- as.numeric(lapply(h, sum))
       templist <- lapply(h, ncol)
       templist[sapply(templist, is.null)] <- NA
@@ -34,12 +42,12 @@ hf.alignment.stats <- function(x, slide=F, width=1000, step=500) {
         n$Fu.F_S[i] <- afufs(n$n.sequences[i], n$n.haplotypes[i], n$pi[i])
       }
     } else {
-      d <- diversity.stats(d, pi=T)
-      d <- neutrality.stats(d, detail=T, do.R2=T)
-      n <- data.frame(get.neutrality(d)[[1]])
+      go <- diversity.stats(go, pi=T)
+      go <- neutrality.stats(go, detail=T, do.R2=T)
+      n <- data.frame(get.neutrality(go)[[1]])
       n$numindividuals <- numindividuals
-      n$pi <- get.diversity(d)[[1]][,3]
-      h <- d@region.stats@haplotype.counts[[1]]
+      n$pi <- get.diversity(go)[[1]][,3]
+      h <- go@region.stats@haplotype.counts[[1]]
       n$n.sequences <- sum(h)
       n$n.haplotypes <- ncol(h)
       n$n.singleton.haplotypes <- length(which(h == 1))
